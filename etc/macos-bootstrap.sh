bb3b49e28100137e1a97835449609aee20a442db
Joseph Heyburn <jdheyburn@gmail.com>
2020-12-02 15:08:39 +0000

    Brew refactor;

--- etc/macos-bootstrap.sh
+++ etc/macos-bootstrap.sh
@@ -11,7 +11,24 @@ defaults write com.apple.TextEdit RichText -int 0
 # bootstrap git too
 
 function validate() {
+    echo "macos-bootstrap.sh validate not yet implemented"
+}
 
+function installBrewPkgs() {
+    if ! which -s brew; then
+        echo "brew not found - installing now"
+        /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    fi
+    echo "brew installed"
+
+    # Brewfile should be sitting in the same directory at this file\
+    local brewPath=$(dirname $(greadlink -f $0))
+    if [ ! -e "$brewPath/Brewfile" ]; then
+        echo "Brewfile not found, aborting brew packages"
+        return
+    fi
+    echo "Installing brew packages"
+    brew bundle --file $brewPath/Brewfile
 }
 
 function installPowerlineFonts() {
@@ -29,6 +46,8 @@ function main() {
     validate
 
     installPowerlineFonts
+
+    installBrewPkgs
 }
 
 main $@
