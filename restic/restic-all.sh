682eb8386f802ee9d4021bbd4578697e12017abb
Joseph Heyburn <jdheyburn@gmail.com>
2021-03-27 23:40:52 +0000

    Restic updates

--- restic/restic-all.sh
+++ restic/restic-all.sh
@@ -3,38 +3,38 @@
 # Master script for backing up anything to do with restic
 
 function backup_small_files {
-    BACKUP_PATHS="/var/lib/unifi/backup/autobackup /etc/pihole /mnt/usb/Backup/media/beets-db"
+    export BACKUP_PATHS="/var/lib/unifi/backup/autobackup /etc/pihole /mnt/usb/Backup/media/beets-db"
     #BACKUP_EXCLUDES="--exclude-file /home/rupert/.restic_excludes --exclude-if-present .exclude_from_backup"
-    BACKUP_EXCLUDES=""
-    RETENTION_DAYS=7
-    RETENTION_WEEKS=5
-    RETENTION_MONTHS=12
-    RETENTION_YEARS=3
-    RESTIC_REPOSITORY=/mnt/usb/Backup/restic/small-files
-    RESTIC_PASSWORD_FILE=/home/restic/.resticpw
+    export BACKUP_EXCLUDES=""
+    export RETENTION_DAYS=7
+    export RETENTION_WEEKS=5
+    export RETENTION_MONTHS=12
+    export RETENTION_YEARS=3
+    export RESTIC_REPOSITORY=/mnt/usb/Backup/restic/small-files
+    export RESTIC_PASSWORD_FILE=/home/restic/.resticpw
     
     echo "Backing up small files"
     restic backup --verbose --tag systemd.timer $BACKUP_EXCLUDES $BACKUP_PATHS
     echo "Forgetting old small files"
-    restic forget --verbose --tag systemd.timer --group-by "paths,tags" --keep-daily $RETENTION_DAYS --k
+    restic forget --verbose --tag systemd.timer --group-by "paths,tags" --keep-daily $RETENTION_DAYS --keep-weekly $RETENTION_WEEKS --keep-monthly $RETENTION_MONTHS --keep-yearly $RETENTION_YEARS
 }
 
 
 function backup_media {
-    BACKUP_PATHS="/mnt/usb/Backup/media/beets-db /mnt/usb/Backup/media/lossless /mnt/usb/Backup/media/music /mnt/usb/Backup/media/vinyl"
+    export BACKUP_PATHS="/mnt/usb/Backup/media/beets-db /mnt/usb/Backup/media/lossless /mnt/usb/Backup/media/music /mnt/usb/Backup/media/vinyl"
     #BACKUP_EXCLUDES="--exclude-file /home/rupert/.restic_excludes --exclude-if-present .exclude_from_backup"
-    BACKUP_EXCLUDES=""
-    RETENTION_DAYS=30
-    RETENTION_WEEKS=5
-    RETENTION_MONTHS=12
-    RETENTION_YEARS=3
-    RESTIC_REPOSITORY=/mnt/usb/Backup/restic/media
-    RESTIC_PASSWORD_FILE=/home/restic/.resticmediapw
+    export BACKUP_EXCLUDES=""
+    export RETENTION_DAYS=30
+    export RETENTION_WEEKS=5
+    export RETENTION_MONTHS=12
+    export RETENTION_YEARS=3
+    export RESTIC_REPOSITORY=/mnt/usb/Backup/restic/media
+    export RESTIC_PASSWORD_FILE=/home/restic/.resticmediapw
     
     echo "Backing up media"
     restic backup --verbose --tag systemd.timer $BACKUP_EXCLUDES $BACKUP_PATHS
     echo "Forgetting old media"
-    restic forget --verbose --tag systemd.timer --group-by "paths,tags" --keep-daily $RETENTION_DAYS --k
+    restic forget --verbose --tag systemd.timer --group-by "paths,tags" --keep-daily $RETENTION_DAYS --keep-weekly $RETENTION_WEEKS --keep-monthly $RETENTION_MONTHS --keep-yearly $RETENTION_YEARS
 }
 
 
