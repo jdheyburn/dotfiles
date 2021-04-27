6f9a193050c88b0ea716ba97b4ec016d7491107a
Joseph Heyburn <jdheyburn@gmail.com>
2021-04-27 19:30:33 +0100

    Add emailer unit

---
+++ restic/systemd/unit-status-mail.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# From https://northernlightlabs.se/2014-07-05/systemd-status-mail-on-unit-failure.html
+
+MAILTO="root"
+MAILFROM="unit-status-mailer"
+UNIT=$1
+
+EXTRA=""
+for e in "${@:2}"; do
+  EXTRA+="$e"$'\n'
+done
+
+UNITSTATUS=$(systemctl status $UNIT)
+
+sendmail $MAILTO <<EOF
+From:$MAILFROM
+To:$MAILTO
+Subject:Status mail for unit: $UNIT
+
+Status report for unit: $UNIT
+$EXTRA
+
+$UNITSTATUS
+EOF
+
+echo -e "Status mail sent to: $MAILTO for unit: $UNIT"
