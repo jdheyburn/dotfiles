c13fcfc63f48316e1e5977efde6a3f182888a290
Joseph Heyburn <jdheyburn@gmail.com>
2021-04-28 06:48:51 +0100

    Add audio output

--- .oh-my-zsh/custom/utils.zsh
+++ .oh-my-zsh/custom/utils.zsh
@@ -3,7 +3,7 @@
 # WIP
 function set-output-device() {
     pactl list short sinks
-    read -p "Enter the output device" newSink
+    vared -p "Enter the output device" -c newSink
     pacmd set-default-sink $newSink
     pactl list short sink-inputs|while read stream; do
         streamId=$(echo $stream|cut '-d ' -f1)
