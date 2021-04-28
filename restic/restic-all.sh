e5ccfa99a93b884429f53710247b726f19492780
Joseph Heyburn <jdheyburn@gmail.com>
2021-04-28 08:10:39 +0100

    Refactor restic-all

--- restic/restic-all.sh
+++ restic/restic-all.sh
@@ -2,9 +2,23 @@
 
 # Master script for backing up anything to do with restic
 
+function do_restic() {
+    local mode=$1
+    local target=$2
+
+    if [ $mode == "backup" ]; then
+        echo "Backing up $target"
+        restic backup --verbose --tag systemd.timer $BACKUP_EXCLUDES $BACKUP_PATHS
+        echo "Forgetting old $target"
+        restic forget --verbose --tag systemd.timer --group-by "paths,tags" --keep-daily $RETENTION_DAYS --keep-weekly $RETENTION_WEEKS --keep-monthly $RETENTION_MONTHS --keep-yearly $RETENTION_YEARS
+    elif [ $mode == "prune" ]; then
+        echo "pruning $target"
+        restic --verbose prune
+    fi
+}
+
 function small_files() {
     export BACKUP_PATHS="/var/lib/unifi/backup/autobackup /etc/pihole /mnt/usb/Backup/lms"
-    #BACKUP_EXCLUDES="--exclude-file /home/rupert/.restic_excludes --exclude-if-present .exclude_from_backup"
     export BACKUP_EXCLUDES=""
     export RETENTION_DAYS=7
     export RETENTION_WEEKS=5
@@ -13,41 +27,24 @@ function small_files() {
     export RESTIC_REPOSITORY=/mnt/usb/Backup/restic/small-files
     export RESTIC_PASSWORD_FILE=/home/restic/.resticpw
 
-    mode=$1
+    local mode=$1
 
-    if [ $mode == "backup" ]; then
-        echo "Backing up small files"
-        restic backup --verbose --tag systemd.timer $BACKUP_EXCLUDES $BACKUP_PATHS
-        echo "Forgetting old small files"
-        restic forget --verbose --tag systemd.timer --group-by "paths,tags" --keep-daily $RETENTION_DAYS --keep-weekly $RETENTION_WEEKS --keep-monthly $RETENTION_MONTHS --keep-yearly $RETENTION_YEARS
-    elif [ $mode == "prune" ]; then
-        echo "pruning small files"
-        restic --verbose prune
-    fi
+    do_restic $mode "small files"
 }
 
 function media() {
     export BACKUP_PATHS="/mnt/usb/Backup/media/beets-db /mnt/usb/Backup/media/lossless /mnt/usb/Backup/media/music /mnt/usb/Backup/media/vinyl"
-    #BACKUP_EXCLUDES="--exclude-file /home/rupert/.restic_excludes --exclude-if-present .exclude_from_backup"
     export BACKUP_EXCLUDES=""
     export RETENTION_DAYS=30
-    export RETENTION_WEEKS=5
-    export RETENTION_MONTHS=12
-    export RETENTION_YEARS=3
+    export RETENTION_WEEKS=0
+    export RETENTION_MONTHS=0
+    export RETENTION_YEARS=0
     export RESTIC_REPOSITORY=/mnt/usb/Backup/restic/media
     export RESTIC_PASSWORD_FILE=/home/restic/.resticmediapw
 
-    mode=$1
+    local mode=$1
 
-    if [ $mode == "backup" ]; then
-        echo "Backing up media"
-        restic backup --verbose --tag systemd.timer $BACKUP_EXCLUDES $BACKUP_PATHS
-        echo "Forgetting old media"
-        restic forget --verbose --tag systemd.timer --group-by "paths,tags" --keep-daily $RETENTION_DAYS --keep-weekly $RETENTION_WEEKS --keep-monthly $RETENTION_MONTHS --keep-yearly $RETENTION_YEARS
-    elif [ $mode == "prune" ]; then
-        echo "pruning media"
-        restic --verbose prune
-    fi
+    do_restic $mode "media"
 }
 
 function main() {
