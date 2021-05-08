7aed004f842e8a5910a778dcceba5512aee2e449
Joseph Heyburn <jdheyburn@gmail.com>
2021-05-08 11:33:34 +0100

    Better set-output-device

--- .oh-my-zsh/custom/utils.zsh
+++ .oh-my-zsh/custom/utils.zsh
@@ -3,7 +3,7 @@
 # WIP
 function set-output-device() {
     pactl list short sinks
-    vared -p "Enter the output device" -c newSink
+    vared -p "Enter the output device: " -c newSink
     pacmd set-default-sink $newSink
     pactl list short sink-inputs|while read stream; do
         streamId=$(echo $stream|cut '-d ' -f1)
