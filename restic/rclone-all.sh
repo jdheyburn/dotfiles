d117e222cadb8b4cfee8446a19ef07fc04851993
Joseph Heyburn <jdheyburn@gmail.com>
2022-04-05 07:27:38 +0100

    Changes made on dee

--- restic/rclone-all.sh
+++ restic/rclone-all.sh
@@ -2,24 +2,29 @@
 
 # Master script for backing up all rclone stuff to various clouds
 
-RCLONE_CONFIG=/home/jdheyburn/.config/rclone/rclone.conf
-
 function main() {
 
+    if [ "$BACKUP_TYPE" = "media" ]; then
+
     echo "rcloning beets-db -> gdrive:media/beets-db"
-    rclone -v sync /mnt/usb/Backup/media/beets-db gdrive:media/beets-db --config=${RCLONE_CONFIG}
+    $RCLONE -v sync /mnt/usb/Backup/media/beets-db gdrive:media/beets-db --config=$RCLONE_CONFIG
 
     echo "rcloning music -> gdrive:media/music"
-    rclone -v sync /mnt/usb/Backup/media/music gdrive:media/music --config=${RCLONE_CONFIG}
+    $RCLONE -v sync /mnt/usb/Backup/media/music gdrive:media/music --config=$RCLONE_CONFIG 
 
     echo "rcloning lossless -> gdrive:media/lossless"
-    rclone -v sync /mnt/usb/Backup/media/lossless gdrive:media/lossless --config=${RCLONE_CONFIG}
+    $RCLONE -v sync /mnt/usb/Backup/media/lossless gdrive:media/lossless --config=$RCLONE_CONFIG
 
     echo "rcloning vinyl -> gdrive:media/vinyl"
-    rclone -v sync /mnt/usb/Backup/media/vinyl gdrive:media/vinyl --config=${RCLONE_CONFIG}
+    $RCLONE -v sync /mnt/usb/Backup/media/vinyl gdrive:media/vinyl --config=$RCLONE_CONFIG
 
-    echo "rcloning restic -> b2:restic"
-    rclone -v sync /mnt/usb/Backup/restic b2:iifu8Noi-backups/restic/ --config=${RCLONE_CONFIG}
+    echo "rcloning restic/media -> b2:restic/media"
+    $RCLONE -v sync /mnt/usb/Backup/restic/media b2:iifu8Noi-backups/restic/media --config=$RCLONE_CONFIG
+
+    elif [ "$BACKUP_TYPE" = "small-files" ]; then
+        echo "rcloning restic/small-files -> b2:restic/small-files"
+        $RCLONE -v sync /mnt/usb/Backup/restic/small-files b2:iifu8Noi-backups/restic/small-files --config=$RCLONE_CONFIG
+    fi
 
     echo "Done rcloning"
 }
